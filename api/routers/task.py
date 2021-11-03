8cf3aaf6be2488bceae6779be4eb639a18b725b8
jackkitte <kakeru.tamashiro@revcomm.co.jp>
2021-11-03 20:46:27 +0900

    crud.createの実装

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,7 +1,10 @@
 from typing import List
 
+import api.cruds.task as task_crud
 import api.schemas.task as task_schema
-from fastapi import APIRouter
+from api.db import get_db
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
@@ -10,8 +13,8 @@ async def list_tasks():
     return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    return await task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(task_id: int, task_body: task_schema.TaskCreate):
