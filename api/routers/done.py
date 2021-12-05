12c4361177b0023b47eb72c5d55ad035f8d8088a
jackkitte <kakeru.tamashiro@revcomm.co.jp>
2021-12-06 01:04:23 +0900

    doneのテスト追加

--- api/routers/done.py
+++ api/routers/done.py
@@ -13,7 +13,7 @@ router = APIRouter()
 async def mark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
     done = await done_crud.get_done(db, task_id)
     if done is not None:
-        raise HTTPException(status_code=404, detail="Done already exists")
+        raise HTTPException(status_code=400, detail="Done already exists")
 
     return await done_crud.create_done(db, task_id)
 
@@ -22,6 +22,6 @@ async def mark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
 async def unmark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
     done = await done_crud.get_done(db, task_id)
     if done is None:
-        raise HTTPException(status_code=404, detail="Done already exists")
+        raise HTTPException(status_code=404, detail="Done not found")
 
     return await done_crud.delete_done(db, done)
